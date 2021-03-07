f2397354c238733afdfd6f37d97e1d9756caa3ff
Sarah Helgen <sarah.helgen@gmail.com>
2021-03-07 08:29:18 -0600

    submit button and addEmployee function working

--- scripts/client.js
+++ scripts/client.js
@@ -1,44 +1,33 @@
-$( document ).ready( onReady );
+$( document ).ready (onReady );
 
-let totalEmployees = []; //created totalEmployees array
+let employeeInfo = [];
 
 function onReady(){
-    $('#submitButton').on( 'click', addEmployee );
+    console.log( 'in JQ');
+    $( '#addSubmitButton ').on( 'click ', addEmployee );
 }
+//end onReady
 
 function addEmployee(){
-    //create object from user inputs
-    let inputEmployee = {
-        firstName: $('#firstNameIn').val(),
-        lastName: $('#lastNameIn').val(),
+    console.log( 'in addEmployee ');
+    //create object with user input
+    let newEmployees = {
+        firstName: $( '#firstNameIn').val(),
+        lastName: $( '#lastNameIn' ).val(),
         id: $( '#idIn').val(),
         title: $( '#titleIn').val(),
-        annualSalary: $('#salaryIn').val()
-    }//end inputEmployee
-    //empty
-    $( '.inputInfo').val('');
-    //push inputEmployee into totalEmployees array
-    totalEmployees.push( inputEmployee );
+        salary: $( '#salaryIn').val()
+    }//end newEmployees
+    //push the object into array
+    employeeInfo.push ( newEmployees );
     //update DOM
-    displayEmployee(totalEmployees);
-}
 
-function displayEmployee(){
-    console.log( 'in displayEmployee ');
-    //target output by ID
-    let el = $( '#table ');
-    //empty
-    el.empty();
-    //loop thru employees array
-    for( i=0; i<totalEmployees.length; i++ ){
-        el.append(` <tr>
-                    <th>${totalEmployees[i].firstName}</th>
-                    <th>${totalEmployees[i].lastName}</th>
-                    <th>${totalEmployees[i].id}</th>
-                    <th>${totalEmployees[i].title}</th>
-                    <th>${totalEmployees[i].annualSalary}</th>
-                    <td><button>Delete</button></td>
-                </tr>`);
+    //empty text input
+    $( '#firstNameIn').val('');
+    $( '#lastNameIn' ).val( '');
+    $( '#idIn').val( '');
+    $( '#titleIn').val( '');
+    $( '#salaryIn').val( '');
 
-    }//end for
-}//end displayEmployees
+}//end addEmployee
+
